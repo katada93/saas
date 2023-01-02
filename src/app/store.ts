8fdbb7848ac1282a67b20cd281d18bf70d8f9578
Muslim Tsokaev <m.tsokaev@automacon.ru>
2023-01-02 20:14:11 +0300

    Настроил prettier и eslint

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/indent */
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
