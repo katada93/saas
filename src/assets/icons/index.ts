9ece0edb51e503dcb70b1120d2a64e85cfc7c68d
Laziyni <soasue@mail.ru>
2023-01-04 13:19:40 +0300

    Реализовал ui компонент для иконок

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,13 @@
+import { ReactComponent as Dashboard } from './dashboard.svg';
+import { ReactComponent as Tasks } from './tasks.svg';
+import { ReactComponent as Contact } from './contacts.svg';
+import { ReactComponent as Logout } from './logout.svg';
+import { ReactComponent as Chat } from './chat.svg';
+
+export const icons = {
+  Dashboard,
+  Tasks,
+  Contact,
+  Logout,
+  Chat,
+};
