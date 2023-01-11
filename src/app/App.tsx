ff19a7b1013c700516aa9781ed3bd83e09e8cc84
Laziyni <soasue@mail.ru>
2023-01-11 20:26:43 +0300

    Реализовал страницу TaskListPage и внес мелкие исправления

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { useState, useCallback } from 'react';
 import clsx from 'clsx';
 
-import { Paper, Icon, Button, Status } from 'shared/ui';
-import { Sidebar, SearchBar } from 'components';
+import { Sidebar } from 'components';
 
 import './styles/index.scss';
-import { TasksTable } from 'components/TasksTable/TasksTable';
+
+import { TaskListPage } from 'pages';
 
 function App() {
   const [isOpenSidebar, setIsOpenSidebar] = useState(true);
@@ -17,19 +17,7 @@ function App() {
   return (
     <div className={clsx('App', { isOpenSidebar })}>
       <Sidebar isOpen={isOpenSidebar} onToggle={handleToggle} />
-      <SearchBar value="" onChange={() => {}} placeholder="Search for task" />
-
-      <div className="content">
-        <Paper>
-          <Icon name="chat" />
-          <Button>CLick</Button>
-
-          <Status>Default</Status>
-          <Status color="success">Scheduled</Status>
-          <Status color="warning">Sent</Status>
-        </Paper>
-        <TasksTable />
-      </div>
+      <TaskListPage />
     </div>
   );
 }
